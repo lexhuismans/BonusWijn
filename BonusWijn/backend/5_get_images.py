4f2228f18e0286a9767370f3809abfd590fbe4d3
WybeKoper <WybeKoper@users.noreply.github.com>
2021-05-11 15:31:55 +0200

    Updated image retrieval

--- BonusWijn/backend/5_get_images.py
+++ BonusWijn/backend/5_get_images.py
@@ -7,15 +7,20 @@ with open("C:/Users/wybek/Documents/school/Master/LexWijn/BonusWijn/BonusWijn/ba
     data = json.load(read_file)
 
 
-for wine in data:
-    # print(wine['images'][2]['url'])
-    print(wine['title'])
-    name = wine['title'].replace(" ", "")
-    link = wine['images'][2]['url']
-    print(link)
-    # urllib.request.urlretrieve(link, "./frontend/assets/images/" + name + ".PNG")
+def recuring(n):
+    return recuring(n-1) + recuring(n-2)
 
-    response = requests.get(link)
-    with open("C:/Users/wybek/Documents/school/Master/LexWijn/BonusWijn/BonusWijn/frontend/myapp/assets/images/" + name + ".PNG", "wb") as write_file:
-        write_file.write(response.content)
-        write_file.close()
+recuring(100)
+
+# for wine in data:
+#     # print(wine['images'][2]['url'])
+#     print(wine['title'])
+#     name = wine['title'].replace(" ", "")
+#     link = wine['images'][2]['url']
+#     print(link)
+#     # urllib.request.urlretrieve(link, "./frontend/assets/images/" + name + ".PNG")
+#
+#     # response = requests.get(link)
+#     # with open("C:/Users/wybek/Documents/school/Master/LexWijn/BonusWijn/BonusWijn/frontend/myapp/assets/images/" + name + ".PNG", "wb") as write_file:
+#     #     write_file.write(response.content)
+#     #     write_file.close()
