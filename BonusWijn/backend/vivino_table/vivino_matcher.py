9fa8334845b0e2a6f9d7d94bb436fec40cd54d3b
Lex Huismans <lexhuismans@outlook.com>
2021-05-20 09:37:06 +0200

    reformat vivino data

--- BonusWijn/backend/vivino_table/vivino_matcher.py
+++ BonusWijn/backend/vivino_table/vivino_matcher.py
@@ -19,9 +19,23 @@ def find_title(title):
                 json.dump(vivino_data, file)
             return vivino_json
 
+# Reformat
+with open('./vivino_data/vivino_data.json', 'r') as file:
+    vivino_data = json.load(file)
+    new_vivino_list = []
+    for item in vivino_data:
+        title = list(item.keys())[0]
+        viv_json = item[title]
+        new_vivino_list.append({'title': title, 'vivino': viv_json})
+
+    with open('./vivino_data/vivino_data_new.json', 'w') as outfile:
+        json.dump(new_vivino_list, outfile)
+
+'''
 with open('./raw_supermarket/jumbo_wines_titles.json', 'r') as file:
     title_list = json.load(file)
     for title in title_list:
         vivino = find_title(title)
         pprint(vivino)
         time.sleep(10)
+'''
