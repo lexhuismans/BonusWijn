2fb47adc5e403122467440a114f993e1b48e187f
Lex Huismans <lexhuismans@outlook.com>
2021-05-22 12:22:00 +0200

    Interface

---
+++ BonusWijn/backend/production/ah.py
@@ -0,0 +1,15 @@
+from supermarktconnector.ah import AHConnector
+
+import math
+import json
+
+def get_from_query(query='wijn'):
+    # Get all from query. 
+
+    return data 
+
+
+def format_for_use(wine):
+    # format wines for use on website
+
+    return wine
