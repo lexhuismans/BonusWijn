7efcada8bdcecfa71387061d7fa2bf1705a6c732
Lex Huismans <lexhuismans@outlook.com>
2021-05-22 15:24:14 +0200

    Add food and reformat

--- BonusWijn/backend/production/vivino.py
+++ BonusWijn/backend/production/vivino.py
@@ -65,63 +65,74 @@ def find_title(title, wine_name_dict):
     '''
     Find vivino wine json in database. 
     '''
-    print(title)
     if title in wine_name_dict:
         print("already contained")
-        return wine_name_dict[title]
+        return filter_vivino_data(wine_name_dict[title])
     else:
-        print('wine not contained yet')
-        vivino_json = get_json_from_query(title)
+        print('Wine not contained yet: ', title)
+        try:
+            vivino_json = get_json_from_query(title)
+        except:
+            vivino_json = None
         wine_name_dict[title] = vivino_json
         # !!Ook nog toevoegen aan database!!!
-        return vivino_json
+        return filter_vivino_data(vivino_json)
 
 
-def add_vivino_data(data):
+def filter_vivino_data(vivino_data):
     '''
     Add formatted vivino data to json. 
     '''
-    combined_data = []
-    for wine in data:
-        title = wine['title']
+    if vivino_data is None:
+        return None
 
-        vivino_data = get_json_from_query(title)
+    filtered_vivino = {}
 
-        # Get rating and number of review
-        wine_data = vivino_data['vintage']['wine']
-        wine['rating'] = wine_data['vintages'][1]['statistics']['ratings_average']
-        wine['numberOfReviews'] = wine_data['vintages'][1]['statistics']['ratings_count']
-        #wine['title'] = vivino_data['name']  # Change to vivino title
-        print(wine['title'])
+    # Get rating and number of review
+    wine_data = vivino_data['vintage']['wine']
+    if int(wine_data['vintages'][0]['statistics']['ratings_count']) > 1000:
+        filtered_vivino['rating'] = wine_data['vintages'][1]['statistics']['ratings_average']
+        filtered_vivino['numberOfReviews'] = wine_data['vintages'][1]['statistics']['ratings_count']
+    else:  # Take all year ratings 
+        filtered_vivino['rating'] = vivino_data['vintage']['statistics']['ratings_average']
+        filtered_vivino['numberOfReviews'] = vivino_data['vintage']['statistics']['ratings_count']
 
-        # Grape
-        try:
-            wine['grape'] = wine_data['grapes'][0]['name']
-            wine['grape_count'] = wine_data['grapes'][0]['wines_count']
-        except IndexError:
-            wine['grape'] = 'Other'
-            wine['grape_count'] = 0
+    # Grape
+    try:
+        filtered_vivino['grape'] = wine_data['grapes'][0]['name']
+        filtered_vivino['grape_count'] = wine_data['grapes'][0]['wines_count']
+    except IndexError:
+        filtered_vivino['grape'] = 'Other'
+        filtered_vivino['grape_count'] = 0
 
-        # Type
-        type = wine_data['type_id']
-        if type == 1:
-            wine['type'] = 'Red'
-        elif type == 2:
-            wine['type'] = 'White'
-        elif type == 3:
-            wine['type'] = 'Bubbles'
-        elif type == 4:
-            wine['type'] = 'Ros\u00e9'
+    # Type
+    type = wine_data['type_id']
+    if type == 1:
+        filtered_vivino['type'] = 'Red'
+    elif type == 2:
+        filtered_vivino['type'] = 'White'
+    elif type == 3:
+        filtered_vivino['type'] = 'Bubbles'
+    elif type == 4:
+        filtered_vivino['type'] = 'Ros\u00e9'
+    else:
+        filtered_vivino['type'] = 'Other'
+
+    # Region
+    try:
+        filtered_vivino['country'] = wine_data['region']['country']['name']
+        filtered_vivino['region'] = wine_data['region']['name']
+    except TypeError:
+        filtered_vivino['country'] = 'Other'
+        filtered_vivino['region'] = 'Other'
+
+    # Food
+    foods = []
+    for food in wine_data['foods']:
+        if 'fish' in food['name']:
+            foods.append('Fish')
         else:
-            wine['type'] = 'Other'
-
-        # Region
-        try:
-            wine['country'] = wine_data['region']['country']['name']
-            wine['region'] = wine_data['region']['name']
-        except TypeError:
-            wine['country'] = 'Other'
-            wine['region'] = 'Other'
-        combined_data.append(wine)
-
-    return combined_data
+            foods.append(food['name'])
+    filtered_vivino['foods'] = foods
+        
+    return filtered_vivino
