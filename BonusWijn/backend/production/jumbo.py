3c2cf1ee6c2e324049ac5db196ef46303b79401a
Lex Huismans <lexhuismans@outlook.com>
2021-05-23 12:56:06 +0200

    Update dict tags to match AH

--- BonusWijn/backend/production/jumbo.py
+++ BonusWijn/backend/production/jumbo.py
@@ -50,25 +50,25 @@ def format_for_use(store_data):
     # bottle size
     if 'quantity' in store_data:
         if store_data['quantity'] in ['750 ml', '75 cl', '750 ml.', '0,75 i', '75 cl.', '0,75 liter', '0,75 l', '750.0 ml', '0.75 liter']:
-            wine_object['bottleSize'] = "0,75 l"
+            wine_object['volume'] = "0,75 l"
         elif store_data['quantity'] in ['250 ml', '250.0 ml', '25 cl.']:
-            wine_object['bottleSize'] = "0,25 l"
+            wine_object['volume'] = "0,25 l"
         elif store_data['quantity'] == '1500.0 ml':
-            wine_object['bottleSize'] = '1,5 l'
+            wine_object['volume'] = '1,5 l'
         elif store_data['quantity'] in ['6 x 750 ml', '6 x 75 cl', '6 x 0,75 l']:
-            wine_object['bottleSize'] = '6 x 0,75 l'
+            wine_object['volume'] = '6 x 0,75 l'
         elif store_data['quantity'] in ['6 x 6000.0 ml', '6 x 1 l']:
-            wine_object['bottleSize'] = '6 x 1 l'
+            wine_object['volume'] = '6 x 1 l'
         elif store_data['quantity'] in ['50 cl']:
-            wine_object['bottleSize'] = '0,5 l'
+            wine_object['volume'] = '0,5 l'
         elif store_data['quantity'] == '20 cl':
-            wine_object['bottleSize'] = '0,2 l'
+            wine_object['volume'] = '0,2 l'
         elif store_data['quantity'] in ['18.7 cl']:
             wine_object['quantity'] = '0,187 l'
         elif store_data['quantity'] == '1 l':
-            wine_object['bottleSize'] = '1 l'
+            wine_object['volume'] = '1 l'
         elif store_data['quantity'] in ['375 ml', '37,5 cl', '375 ml.']:
-            wine_object['bottleSize'] = '0,375 l'
+            wine_object['volume'] = '0,375 l'
         #else:
             #print('Skip: ', store_data['title'])
     else:
@@ -77,8 +77,8 @@ def format_for_use(store_data):
 
     # Prices
     price = format_price(store_data['prices']['price']['amount'])
-    wine_object['originalPrice'] = price
-    wine_object['amountOfBottles'] = 1
+    wine_object['original_price_per_fles'] = price
+    wine_object['amount_of_bottles'] = 1
     
     # Promotion
     if 'promotion' in store_data:
@@ -90,38 +90,38 @@ def format_for_use(store_data):
         else:
             wine_object['promotionType'] = 'normal'
         if promotion == '3 halen, 2 betalen':
-            wine_object['bonusPrice'] = 2 * price / 3
-            wine_object['amountOfBottles'] = 3
+            wine_object['bonus_price_per_fles'] = 2 * price / 3
+            wine_object['amount_of_bottles'] = 3
         elif promotion == '2 halen, 1 betalen':
-            wine_object['bonusPrice'] = price / 2
-            wine_object['amountOfBottles'] = 2
+            wine_object['bonus_price_per_fles'] = price / 2
+            wine_object['amount_of_bottles'] = 2
         elif promotion == "2 voor 7,99 euro":
-            wine_object['bonusPrice'] = 7.99 / 2
-            wine_object['amountOfBottles'] = 2
+            wine_object['bonus_price_per_fles'] = 7.99 / 2
+            wine_object['amount_of_bottles'] = 2
         elif promotion == '3 voor 10,00 euro':
-            wine_object['bonusPrice'] = 10 / 3
-            wine_object['amountOfBottles'] = 3
+            wine_object['bonus_price_per_fles'] = 10 / 3
+            wine_object['amount_of_bottles'] = 3
         elif promotion == '1 voor 4,49 euro':
-            wine_object['bonusPrice'] = 4.49
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = 4.49
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '1 voor 2,99 euro':
-            wine_object['bonusPrice'] = 2.99
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = 2.99
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '1 voor 6,99 euro':
-            wine_object['bonusPrice'] = 6.99
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = 6.99
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '10% korting':
-            wine_object['bonusPrice'] = price * .9
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = price * .9
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '25% korting':
-            wine_object['bonusPrice'] = price * .75
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = price * .75
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '50% korting':
-            wine_object['bonusPrice'] = price * .5
-            wine_object['amountOfBottles'] = 1
+            wine_object['bonus_price_per_fles'] = price * .5
+            wine_object['amount_of_bottles'] = 1
         elif promotion == '4 halen 2 betalen':
-            wine_object['bonusPrice'] = price * 2
-            wine_object['amountOfBottles'] = 4
+            wine_object['bonus_price_per_fles'] = price * 2 / 4
+            wine_object['amount_of_bottles'] = 4
         else:
             print('Promotion niet gevonden:')
             print(promotion)
@@ -129,6 +129,6 @@ def format_for_use(store_data):
         wine_object['bonus'] = False
     
     if '6 x' in store_data['title'] or '6x' in store_data['title']:
-        wine_object['amountOfBottles'] = 6
+        wine_object['amount_of_bottles'] = 6
 
     return wine_object
